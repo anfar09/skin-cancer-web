c6b52a9ccec3fc9cc4b5d441882aba5e513017c1
unknown <anfar956@gmail.com>
2026-02-08 14:54:42 +0700

    deploy web

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def save_result():
     return jsonify({"status": "saved", "image": image_filename})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=10000)
